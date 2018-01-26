0e0409a55f3a1a992233424a4d441a3d0d4cfdca
Kissor Jeyabalan <git@kij.no>
2018-01-26 22:34:32 +0100

    added appstate function

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,45 @@
-import * as fb from 'facebook-chat-api';
+import * as chat from 'facebook-chat-api';
+import * as fs from 'fs';
+import { setTimeout } from 'timers';
+import { Configuration } from './config/Configuration';
+import { ConnectionManager } from './db/ConnectionManager';
 
-const creds: fb.LoginCredentials = {email: 'email', password: 'password'};
+ConnectionManager.getInstance().openConnection();
+const config = Configuration.getInstance();
 
-fb(creds, (err: fb.Error, api: fb.Api) => {
-    if (err) { return console.error(err); }
+const options: chat.ApiOptions = {
+    logLevel: config.fetch('login.logLevel'),
+    selfListen: config.fetch('login.selfListen'),
+    listenEvents: config.fetch('login.listenEvents'),
+    pageID: config.fetch('login.pageID'),
+    updatePresence: config.fetch('login.updatePresence'),
+    forceLogin: config.fetch('login.forceLogin'),
+};
 
-    api.listen((error: fb.Error, message: fb.MessageEvent) => {
-        if (error) { return console.error(error); }
+const creds: chat.LoginCredentials | chat.AppState = hasAppState() ? {
+    appState: JSON.parse(fs.readFileSync(config.fetch('app.state'), 'utf-8')),
+} : {
+    email: config.fetch('account.username'),
+    password: config.fetch('account.password'),
+};
 
-        console.log(`${message.senderID}: ${message.body}`);
-        console.log(`Sent to thread: ${message.threadID}`);
-    });
+chat(creds, options, (err: chat.Error, api: chat.Api) => {
+    if (err) {
+        switch (err.error) {
+            case 'login-approval':
+                console.log(`Please verify your login externally. You have ${config.fetch('login.timeout')}s to approve it.`);
+                setTimeout(() => err.continue(), config.fetch('login.timeout') * 1000);
+
+                return;
+            default:
+                console.log('Could not log in. Shutting down...');
+                ConnectionManager.getInstance().closeConnection();
+                process.exit();
+        }
+    }
+    fs.writeFileSync(config.fetch('app.state'), JSON.stringify(api.getAppState()));
 });
+
+function hasAppState(): boolean {
+    return fs.existsSync(config.fetch('app.state'));
+}
