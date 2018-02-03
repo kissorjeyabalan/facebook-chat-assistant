1bff499e094c380cfe062c11e7466b17fad81a31
Kissor Jeyabalan <git@kij.no>
2018-02-03 21:26:57 +0100

    run scheduler

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import * as chat from 'facebook-chat-api';
 import * as fs from 'fs';
+import * as _ from 'lodash';
 import { setTimeout } from 'timers';
 import { Configuration } from './config/Configuration';
 import { ConnectionManager } from './db/ConnectionManager';
 import { Global } from './Global';
+import ScheduleHandler from './module/handler/ScheduleHandler';
 import { MessageParser } from './parser/MessageParser';
+import { ImageUtil } from './util/ImageUtil';
+import { Reddit } from './util/Reddit';
 
 ConnectionManager.getInstance().openConnection();
 const config = Configuration.getInstance();
@@ -44,9 +48,13 @@ chat(creds, options, (err: chat.Error, api: chat.Api) => {
     fs.writeFileSync(config.fetch('account.state'), JSON.stringify(api.getAppState()));
     Global.getInstance().setApi(api);
 
+    const sh: ScheduleHandler = new ScheduleHandler();
+    sh.start();
+
     api.listen((error: chat.Error, message: chat.MessageEvent) => {
         if (message.senderID != config.fetch('bot.id')) {
             mp.handle(message);
         }
     });
+
 });
