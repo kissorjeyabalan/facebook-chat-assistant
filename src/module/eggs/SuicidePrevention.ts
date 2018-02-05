43aaa7978009de5726705fc2e7110634ee0ba285
Kissor Jeyabalan <git@kij.no>
2018-02-05 17:32:29 +0100

    changed to inside joke instead

--- src/module/eggs/SuicidePrevention.ts
+++ src/module/eggs/SuicidePrevention.ts
@@ -1,20 +1,24 @@
 import { Promise } from 'bluebird';
-import { Api, MessageEvent } from 'facebook-chat-api';
+import { Api, Error, MessageEvent } from 'facebook-chat-api';
 import { Global } from '../../Global';
 import EasterEgg from '../EasterEgg';
 
 export default class SuicidePrevention extends EasterEgg {
-    protected regex: RegExp = /(^(?=.*\bfeeling\b)(?=.*\bsuicidal\b).*$)|(^(?=.*\bto\b)(?=.*\bkill\b)(?=.*\bmyself\b).*$)/i;
+    protected regex: RegExp = /(^(?=.*\bfeeling\b)(?=.*\bsuicidal\b).*$)|((?=.*\bkill(ing?)\b)(?=.*\bmyself\b).*$)/i;
 
     public handleEgg(msg: MessageEvent): any {
         const api = Global.getInstance().getApi();
 
-        let message = 'If you are thinking about killing yourself, you may want to speak with someone...';
+        /*let message = 'If you are thinking about killing yourself, you may want to speak with someone...';
         message += '\n\nMental Helse: 116 123';
         message += '\nRøde Kors: 800 33 321';
         message += '\nKirkens SOS: 22 40 00 40';
 
-        api.sendMessage(message, msg.threadID);
+        api.sendMessage(message, msg.threadID);*/
+
+        api.setMessageReaction(':love:', msg.messageID, (err: Error) => {
+            api.sendMessage('Me too buddy, me too...', msg.threadID);
+        });
 
         return Promise.resolve(msg);
     }
