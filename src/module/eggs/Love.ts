66063ffb0660c74329d93ac6adf6eca71fa861e0
Kissor Jeyabalan <kissorjeyabalan@users.noreply.github.com>
2018-02-06 23:30:47 +0100

    Create Love.ts

---
+++ src/module/eggs/Love.ts
@@ -0,0 +1,17 @@
+import { Promise } from 'bluebird';
+import { Api, Error, MessageEvent } from 'facebook-chat-api';
+import { Global } from '../../Global';
+import EasterEgg from '../EasterEgg';
+
+export default class SuicidePrevention extends EasterEgg {
+    protected regex: RegExp = /(kill me)|(me too thanks)|(fuck life)/i;
+
+    public handleEgg(msg: MessageEvent): any {
+        const api = Global.getInstance().getApi();
+
+
+        api.setMessageReaction(':love:', msg.messageID);
+
+        return Promise.resolve(msg);
+    }
+}
