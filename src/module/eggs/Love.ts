8bd76cb14ec5d0e22ce0fe85e43e787b3b0a21ff
Kissor Jeyabalan <kissorjeyabalan@users.noreply.github.com>
2018-02-06 23:31:47 +0100

    Update Love.ts

--- src/module/eggs/Love.ts
+++ src/module/eggs/Love.ts
@@ -3,7 +3,7 @@ import { Api, Error, MessageEvent } from 'facebook-chat-api';
 import { Global } from '../../Global';
 import EasterEgg from '../EasterEgg';
 
-export default class SuicidePrevention extends EasterEgg {
+export default class Love extends EasterEgg {
     protected regex: RegExp = /(kill me)|(me too thanks)|(fuck life)/i;
 
     public handleEgg(msg: MessageEvent): any {
