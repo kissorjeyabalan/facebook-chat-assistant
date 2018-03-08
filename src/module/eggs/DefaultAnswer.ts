6abf0945d31da187a4f6ebd34e34410b9f9366be
Kissor Jeyabalan <git@kij.no>
2018-03-08 18:05:27 +0100

    added some default answers

--- src/module/eggs/DefaultAnswer.ts
+++ src/module/eggs/DefaultAnswer.ts
@@ -14,6 +14,23 @@ export default class Love extends EasterEgg {
             return msg;
         }
 
+        const didNotUnderstandArr = [
+            'I don\'t understand what you mean.',
+            'Did you have a stroke?',
+            'I can\'t even...',
+            'I can hear you, but I can\'t understand you.',
+            'What?',
+        ];
+
+        const notSpecific = [
+            'I don\'t even know where to begin to answer that.',
+            'You don\'t want me to answer that.',
+            'I\'d rather not.',
+            'I don\'t want to answer that.',
+            'I\'d answer that, but you probably won\'t read it.',
+            'TL;DR',
+        ];
+
         const api = Global.getInstance().getApi();
         const key = Configuration.getInstance().fetch('wolfram.key');
         const uri = `https://api.wolframalpha.com/v2/result?appid=${key}&i=${encodeURIComponent(query)}`;
@@ -31,9 +48,13 @@ export default class Love extends EasterEgg {
         .on('response', (res) => {
             res.on('data', (data) => {
                 if (data.toString() === 'Wolfram|Alpha did not understand your input') {
-                    api.sendMessage('I don\'t understand what you mean.', msg.threadID);
+                    api.sendMessage(didNotUnderstandArr[this.getRndNum(didNotUnderstandArr.length)], msg.threadID);
                 } else if (data.toString() === 'No short answer available') {
-                    api.sendMessage('I don\'t even know where to begin to answer that.', msg.threadID);
+                    api.sendMessage(notSpecific[this.getRndNum(notSpecific.length)], msg.threadID);
+                } else if (data.toString() === 'My name is Wolfram Alpha.') {
+                    api.sendMessage(`My name is ${Configuration.getInstance().fetch('bot.name.nick')}.`, msg.threadID);
+                } else if (data.toString() === 'I was created by Stephen Wolfram and his team.') {
+                    api.sendMessage('I am my own creator.', msg.threadID);
                 } else {
                     api.sendMessage(data.toString(), msg.threadID);
                 }
@@ -41,5 +62,9 @@ export default class Love extends EasterEgg {
         });
 
         return Promise.resolve(msg);
+    }
+
+    private getRndNum(length: number): number {
+        return Math.floor(Math.random() * length);
     }
 }
