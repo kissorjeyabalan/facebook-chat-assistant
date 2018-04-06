d450399b4bbfb7f8acff350473fb9c5a0c6c193d
Kissor Jeyabalan <git@kij.no>
2018-04-06 22:12:31 +0200

    hp item

---
+++ src/module/schedule/HappyBirthday.ts
@@ -0,0 +1,35 @@
+import * as fb from 'facebook-chat-api';
+import * as ns from 'node-schedule';
+import { Global } from '../../Global';
+import Schedule from '../Schedule';
+
+export default class HappyBirthday extends Schedule {
+  private dirRoot: string = `${__dirname}/../..`;
+  public start(): void {
+    const date1 = new Date(2018, 4, 14, 10, 2);
+    const date2 = new Date(2018, 4, 22, 10, 2);
+    const p1 = ns.scheduleJob(date1, () => {
+      const api: fb.Api = Global.getInstance().getApi();
+      const hpmsg: any = {
+        body: 'Happy Birthday, Casey! 23 is pretty old, man. Get your life together.',
+        mentions: [{
+          tag: 'Casey',
+          id: 1616826839,
+        }],
+      };
+      api.sendMessage(hpmsg, '1420517794899222');
+    });
+
+    const p2 = ns.scheduleJob(date2, () => {
+      const api: fb.Api = Global.getInstance().getApi();
+      const hpmsg: any = {
+        body: 'Happy Birthday, Synne! 23 already? Wow. Sad.',
+        mentions: [{
+          tag: 'Synne',
+          id: 584111931,
+        }],
+      };
+      api.sendMessage(hpmsg, '1420517794899222');
+    });
+  }
+}
