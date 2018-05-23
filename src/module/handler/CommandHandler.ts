bf8995fb112e0b7fb4077da613b9b3d1dcef56f8
Kissor Jeyabalan <git@kij.no>
2018-05-23 19:20:27 +0200

    send command to handler if bot doesnt understand

--- src/module/handler/CommandHandler.ts
+++ src/module/handler/CommandHandler.ts
@@ -14,7 +14,7 @@ export default class CommandHandler extends Handler {
     private commandInstances: Map<string, Command>;
     private uh: UserHelper = UserHelper.getInstance();
 
-    private constructor() {
+    public constructor() {
         super();
         this.trigger = Configuration.getInstance().fetch('bot.trigger');
 
