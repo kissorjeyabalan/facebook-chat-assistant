fdfd73d56ccaff4c57db5a17b9238a5c6855200c
Kissor Jeyabalan <git@kij.no>
2018-08-21 12:24:50 +0200

    Removed command name from message

--- src/module/command/Psa.ts
+++ src/module/command/Psa.ts
@@ -6,17 +6,16 @@ import { HelpDetails } from '../HelpDetails';
 export default class ChangeEmoji extends Command {
     public help: HelpDetails = new HelpDetails('PSA', 'psa', 'PSA',
     'Sends a Public Service Announcement to all conversations the bot is a part of.',
-    'psa <content>', 'psa you have been warned', false, false);
+    'psa <message>', 'psa you have been warned', false, false);
 
     public isValid(args: string): boolean {
         const split = args.split(' ');
-
         return split.length > 0;
     }
 
     public run(msg: MessageEvent, psa: string): Promise<any> {
-        const api = Global.getInstance().getApi();
-        api.sendMessage(psa, '1623275961054128');
+	const api = Global.getInstance().getApi();
+        api.sendMessage(`PSA: ${msg.body.substring(5).trim()}`, '1420517794899222');
         return Promise.resolve(msg);
     }
 
