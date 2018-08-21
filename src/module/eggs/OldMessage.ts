ae0c241a5f7d01e25641624528225decf1a08de8
Kissor Jeyabalan <git@kij.no>
2018-08-21 12:25:46 +0200

    Ability to specify amount of messages

--- src/module/eggs/OldMessage.ts
+++ src/module/eggs/OldMessage.ts
@@ -10,19 +10,39 @@ import * as sm from '../../db/model/SavedMessage';
 import { IGroupInfo } from '../../db/model/GroupInfo';
 
 export default class OldMessage extends EasterEgg {
-    protected regex: RegExp = /^[rm]{2}$/i;
+    protected regex: RegExp = /(^[rm]{2}$)|(^rm [0-9]$)/i;
     private gh: GroupHelper = GroupHelper.getInstance();
     
     public handleEgg(msg: fb.MessageEvent): any {
-        const api = Global.getInstance().getApi();
+	
+	let z = 1;
+	//console.log(msg.body);
+	const x = msg.body.split("rm ");
+	console.log(x);
+	if (x.length == 2) {
+	z = parseInt(x[1]);
+	}	
 
-        /*sm.getModel(msg).count({}, (err, count) => {
+	for(let i = 0; i < z; i++) {
+	this.sendMessage(msg);
+        }
+        return Promise.resolve(msg);
+    }
+
+    public sendMessage(msg) {
+	const api = Global.getInstance().getApi();
+
+	sm.getModel(msg).count({}, (err, count) => {
             if (!err) {
                 const random = Math.floor(Math.random() * count);
                 sm.getModel(msg).findOne({}).skip(random).exec((err, res: sm.SavedMessageModel) => {
                     if (!err) {
                         this.gh.getGroupInfo(msg.threadID, (err, info: IGroupInfo) => {
                             if (!err) {
+                        if (res.message.toLowerCase().startsWith("emol") || res.message.toLowerCase().startsWith("r ") || res.message.toLowerCase() == 'rm' || res.message.toLowerCase() == 'where is everyone' || res.message.toLowerCase().startsWith("x ")) {
+        this.sendMessage(msg);
+        return;
+}
                                 const name = info.names[res.senderID];
                                 const time = new Date(res.timestamp);
                                 api.sendMessage(`${res.message}\n-- ${name}`, msg.threadID);
@@ -31,10 +51,8 @@ export default class OldMessage extends EasterEgg {
                     }
                 });
             }
-        });*/
+        });
 
-        api.sendMessage('This command has been disabled.', msg.threadID);
-
-        return Promise.resolve(msg);
     }
+
 }
