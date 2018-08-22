5455ab5bae2c7f6645910f1519db14da8e00188b
Kissor Jeyabalan <git@kij.no>
2018-08-22 10:03:38 +0200

    debug

--- src/module/eggs/WhereIsEveryone.ts
+++ src/module/eggs/WhereIsEveryone.ts
@@ -36,6 +36,7 @@ export default class WhereIsEveryone extends EasterEgg {
         const locs = await this.fmf.getAllLocations();
 
         return new Promise(async resolve => {
+            console.log(locs.toString());
             for (const i in locs) {
                 if (locs[i].location != undefined) {
                     let zip = '';
