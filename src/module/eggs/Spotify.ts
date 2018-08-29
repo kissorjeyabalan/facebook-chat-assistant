e6193b17b5d484a28f48cd0b64faa8d5d5376db7
Kissor Jeyabalan <git@kij.no>
2018-08-29 08:24:06 +0200

    fixed syntax

--- src/module/eggs/Spotify.ts
+++ src/module/eggs/Spotify.ts
@@ -1,23 +1,22 @@
 import * as fb from 'facebook-chat-api';
+import * as fs from 'fs';
 import * as _ from 'lodash';
 import * as request from 'request';
 import { Configuration } from '../../config/Configuration';
 import { Global } from '../../Global';
 import EasterEgg from '../EasterEgg';
-import * as fs from 'fs';
 
 export default class Spotify extends EasterEgg {
-    protected regex: RegExp = /emol play *(.+)/i;
-	private spotify = new (require("spotify-web-api-node"))({
-		"clientId": '',
-		"clientSecret": ''
+	protected regex: RegExp = /emol play *(.+)/i;
+
+	private spotify: any = new (require('spotify-web-api-node'))({
+		clientId: 'clientId',
+		clientSecret: 'clientSecret',
 	});
+
 	private dirRoot: string = `${__dirname}/../..`;
 
-    private getRndNum(length: number): number {
-        return Math.floor(Math.random() * length);
-    }
-	
+
 	public async handleEgg(msg: fb.MessageEvent): Promise<any> {
 		const api = Global.getInstance().getApi();
 		const regex = /emol play *(.+)/i;
@@ -29,7 +28,7 @@ export default class Spotify extends EasterEgg {
 		this.logInSpotify((err) => {
 			if (!err) {
 				const query = msg.body.match(regex)[1].toLowerCase();
-				spotify.searchTracks(query, {}, (err, data) => {
+				this.spotify.searchTracks(query, {}, (err, data) => {
 					if (!err) {
 						const bestMatch = data.body.tracks.items[0];
 						if (bestMatch) {
@@ -39,12 +38,14 @@ export default class Spotify extends EasterEgg {
 							if (preview) {
 								request(preview).pipe(fs.createWriteStream(`${this.dirRoot}/media/temp.mp3}`)).on('close', (err, data) => {
 									if (!err) {
-										const audioMessage: AttachmentMessage = {
-											body: "",
+										const audioMessage: fb.AttachmentMessage = {
+											body: '',
 											attachment: fs.createReadStream(`${this.dirRoot}/media/temp.mp3}`),
 										};
 										api.sendMessage(audioMessage, msg.threadID, (err, data) => {
-											fs.unlink(`${this.dirRoot}/media/temp.mp3`);
+											fs.unlink(`${this.dirRoot}/media/temp.mp3`, (err) => {
+												console.log(err);
+											});
 										});
 									} else {
 										api.sendMessage('failed to download :(((((');
@@ -62,10 +63,10 @@ export default class Spotify extends EasterEgg {
 		});
 	}
 	
-	private logInSpotify(callback = () => {}) {
+	private logInSpotify(callback: any = () => {}) {
 		this.spotify.clientCredentialsGrant({}, (err, data) => {
 			if (!err) {
-				spotify.setAccessToken(data.body.access_token);
+				this.spotify.setAccessToken(data.body.access_token);
 				callback();
 			} else {
 				callback(err);
